f54602f0ff9915b077fe35c570e168044f0b257c
Federico Katzaroff <fkatzaroff@gmail.com>
2020-05-26 15:02:54 -0300

    Add port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import Ahorcado from './models/Ahorcado';
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const gameMap: { [key: string]: Ahorcado } = {}
 
