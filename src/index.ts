eb5248e336c60a29cad910a31e8bf85476227e9c
Federico Katzaroff <fkatzaroff@gmail.com>
2020-06-22 20:33:03 -0300

    Add coverage to test

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,50 @@
-import express from 'express'
+import express from 'express';
 import Ahorcado from './models/Ahorcado';
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-const gameMap: { [key: string]: Ahorcado } = {}
+const gameMap: { [key: string]: Ahorcado } = {};
 
 app.post('/:nick', (req, res) => {
-    if (gameMap[req.params.nick]) { res.send("Name already taken") }
-    else {
-        const game = new Ahorcado();
-        game.setNick(req.params.nick);
-        gameMap[req.params.nick] = game;
-        res.send("Nick set: " + req.params.nick)
-    }
-})
+  if (gameMap[req.params.nick]) {
+    res.send('Name already taken');
+  } else {
+    const game = new Ahorcado();
+    game.setNick(req.params.nick);
+    gameMap[req.params.nick] = game;
+    res.send('Nick set: ' + req.params.nick);
+  }
+});
 
 app.post('/:nick/start', (req, res) => {
-    gameMap[req.params.nick].setRandomWord();
-    res.send(gameMap[req.params.nick].getState())
-})
+  gameMap[req.params.nick].setRandomWord();
+  res.send(gameMap[req.params.nick].getState());
+});
 
 app.post('/:nick/guess/:letter', (req, res) => {
-    gameMap[req.params.nick].tryLetter(req.params.letter);
-    res.send(gameMap[req.params.nick].getState())
-})
+  gameMap[req.params.nick].tryLetter(req.params.letter);
+  res.send(gameMap[req.params.nick].getState());
+});
 
-app.post('/:nick/guessWord/:word', (req, res) => {
-    gameMap[req.params.nick].tryWord(req.params.word);
-    res.send(gameMap[req.params.nick].getState())
-})
+app.post('/:nick/guessword/:word', (req, res) => {
+  gameMap[req.params.nick].tryWord(req.params.word);
+  res.send(gameMap[req.params.nick].getState());
+});
 
 app.post('/:nick/reset', (req, res) => {
-    gameMap[req.params.nick].reset();
-    res.send(gameMap[req.params.nick].getState())
-})
+  gameMap[req.params.nick].reset();
+  res.send(gameMap[req.params.nick].getState());
+});
 
-app.get('/:nick', (req, res) => res.send(gameMap[req.params.nick] ? gameMap[req.params.nick].getState() : "Nick doesn't exist"))
+app.get('/:nick', (req, res) =>
+  res.send(
+    gameMap[req.params.nick]
+      ? gameMap[req.params.nick].getState()
+      : "Nick doesn't exist",
+  ),
+);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`),
+);
